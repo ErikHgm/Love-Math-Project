e42efdb82d2d45586b5f47baa87c55a026a0b455
Erik <erikhgm@gmail.com>
2022-05-22 11:42:25 +0000

    Display addition  question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}!`)
+                runGame(gameType);
             }
         })
+        runGame('addition');
+
     }
-    runGame(gameType);
 })
 /**
  * main game loop function called when the script is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
-    let num1 = Math.floor(Math.random()*25)+1;
-    let num2 = Math.floor(Math.random()*25)+1;
-   
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'subraction') {
+        displaySubtractQuestion(num1, num2);
+    }else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 // parse the input from user 
@@ -45,8 +55,11 @@ function incrementWrongAnswer() {
 }
 
 //generates and displays a random addition question
-function displayAdditionQuestion() {
-   
+function displayAdditionQuestion(num1,num2) {
+    document.getElementById('operand1').textContent  = num1;
+    document.getElementById('operand2').textContent  = num2;
+    document.getElementById('operator').textContent  = '+';
+
 }
 //generates and displays a random subtract question
 function displaySubtractQuestion() {
