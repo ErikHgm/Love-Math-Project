f48b13d904fb2325f9447b05805f9dcde8fcfd55
Erik <erikhgm@gmail.com>
2022-05-22 12:31:51 +0000

    Add code for calculate correct addition answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'subraction') {
         displaySubtractQuestion(num1, num2);
-    }else {
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -41,7 +41,16 @@ function checkAnswer() {
 
 // calculates the correct answer based on the arithmetic chosen
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator: ${operator}`);
+        throw `Unimplemented operator: ${operator}. Aborting!`;
+    }
 }
 
 //checks if answer is correct and adds to the correct answer score tally
@@ -55,10 +64,10 @@ function incrementWrongAnswer() {
 }
 
 //generates and displays a random addition question
-function displayAdditionQuestion(num1,num2) {
-    document.getElementById('operand1').textContent  = num1;
-    document.getElementById('operand2').textContent  = num2;
-    document.getElementById('operator').textContent  = '+';
+function displayAdditionQuestion(num1, num2) {
+    document.getElementById('operand1').textContent = num1;
+    document.getElementById('operand2').textContent = num2;
+    document.getElementById('operator').textContent = '+';
 
 }
 //generates and displays a random subtract question
